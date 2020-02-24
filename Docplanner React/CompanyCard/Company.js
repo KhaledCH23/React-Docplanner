a14f2e96c34dd158a12878b9fe1eec0a21e5c556
KhaledCH23 <60884654+KhaledCH23@users.noreply.github.com>
2020-02-24 21:15:17 +0100

    Update Company.js

--- Docplanner React/CompanyCard/Company.js
+++ Docplanner React/CompanyCard/Company.js
@@ -37,25 +37,4 @@ const Company = props => {
   );
 };
 
-// const CompanyItem = props => <h4>{el.title}</h4>
-
-// const Company = () => (
-//   <div className="company-card">
-//     <div className="title">
-//       <CompanyItem item={title} />
-//       <CompanyItem item={title1} />
-//       <CompanyItem item={title2} />
-//     </div>
-//     <div className="company">
-//       <ul>
-//         <CompanyItem1 item={svg1} />
-//         <CompanyItem1 item={svg2} />
-//         <CompanyItem1 item={svg3} />
-//         <CompanyItem1 item={svg4} />
-//         <CompanyItem1 item={svg5} />
-//         <CompanyItem1 item={svg6} />
-//       </ul>
-//     </div>
-//   </div>
-// );
 export default Company;
